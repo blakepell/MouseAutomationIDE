044cd56c78e343960adc603e72d30fa9d50801e2
Blake Pell <blakepell@hotmail.com>
2022-07-10 16:44:04 -0400

    Ugly reflection code.

--- MouseAutomation/Pages/LuaSyntaxHelp.xaml.cs
+++ MouseAutomation/Pages/LuaSyntaxHelp.xaml.cs
@@ -7,9 +7,12 @@
  * @license           : Closed Source
  */
 
+using System.Linq;
+using System.Text;
 using System.Windows;
 using Argus.Memory;
 using MouseAutomation.Common;
+using MouseAutomation.Lua;
 
 namespace MouseAutomation.Pages
 {
@@ -27,7 +30,100 @@ namespace MouseAutomation.Pages
 
         private void LuaSyntaxHelpPage_OnLoaded(object sender, RoutedEventArgs e)
         {
-            WebBrowser.NavigateToString(@"<html><body style='background: #272F33; color: White; font-family: Segoe UI; padding-top: 10px;'><h2>Lua Syntax Documentation</h2><hr /></body></html>");
+            var sb = new StringBuilder();
+            sb.Append("<html><body style='background: #272F33; color: White; font-family: Segoe UI; padding-top: 10px;'>");
+            sb.Append("<h2>Lua Syntax Documentation</h2><hr />");
+
+            sb.Append("<h3>Mouse Methods</h3>");
+            sb.Append("<table style='width: 100%;'>");
+
+            var t = typeof(MouseScriptCommands);
+
+            // This should get all of our methods but exclude ones that are defined on
+            // object like ToString, GetHashCode, Equals, etc.
+            foreach (var method in t.GetMethods().Where(m => !m.IsSpecialName && m.DeclaringType != typeof(object)).OrderBy(m => m.Name))
+            {
+                sb.Append("<tr>");
+                sb.Append("<td style='vertical-align: top;'>mouse.");
+
+                var parameterDescriptions = string.Join
+                    (", ", method.GetParameters()
+                         .Select(x => $"{x.ParameterType} {x.Name}")
+                         .ToArray());
+
+                sb.AppendLine($"{method.Name}({parameterDescriptions})");
+
+                // Cleanup the parameters to make them easier to read.
+                sb.Replace("System.String", "string")
+                    .Replace("System.Int32", "number")
+                    .Replace("System.DataTime", "datetime")
+                    .Replace("System.Boolean", "boolean")
+                    .Replace("System.Collections.Generic", "")
+                    .Replace("System.Object", "object")
+                    .Replace("Avalon.Common.Models.", "")
+                    .Replace("MahApps.Metro.IconPacks.", "");
+
+                // Remove any double line breaks if they exist.
+                sb.Replace("\r\n\r\n", "\r\n");
+
+                sb.Append("</td>");
+                sb.Append("<td style='vertical-align: top;'>");
+                sb.AppendLine($"Returns {method.ReturnType}<br />");
+                sb.Replace("System.Void", "nothing");
+
+                if (method.CustomAttributes.Any())
+                {
+                    var attr = method.CustomAttributes.FirstOrDefault(x => x.AttributeType.Name.Contains("DescriptionAttribute"));
+
+                    if (attr?.ConstructorArguments.Count > 0)
+                    {
+                        sb.AppendLine(attr.ConstructorArguments[0].ToString().Trim('"'));
+                    }
+                }
+
+                sb.Append("</td></tr>");
+            }
+            sb.Append("</table>");
+
+            sb.Append("<h3>Mouse Properties</h3>");
+            sb.Append("<table style='width: 100%;'>");
+
+            foreach (var prop in t.GetProperties().Where(m => !m.IsSpecialName && m.DeclaringType != typeof(object)).OrderBy(m => m.Name))
+            {
+                sb.AppendLine($"<td>mouse.{prop.Name}</td><td>Gets or sets {prop.PropertyType}");
+                sb.Replace("System.Void", "nothing");
+
+                if (prop.CustomAttributes.Any())
+                {
+                    var attr = prop.CustomAttributes.FirstOrDefault(x => x.AttributeType.Name.Contains("DescriptionAttribute"));
+
+                    if (attr?.ConstructorArguments.Count > 0)
+                    {
+                        sb.AppendLine("<br >").Append(attr.ConstructorArguments[0].ToString().Trim('"'));
+                    }
+                }
+
+                sb.Append("</td></tr>");
+
+                // Cleanup the parameters to make them easier to read.
+                sb.Replace("System.String", "string")
+                    .Replace("System.Int32", "number")
+                    .Replace("System.DataTime", "datetime")
+                    .Replace("System.Boolean", "boolean")
+                    .Replace("System.Collections.Generic", "")
+                    .Replace("System.Object", "object")
+                    .Replace("Avalon.Common.Models.", "")
+                    .Replace("MahApps.Metro.IconPacks.", "");
+
+                // Remove any double line breaks if they exist.
+                sb.Replace("\r\n\r\n", "\r\n");
+            }
+
+            sb.Append("</table>");
+
+            sb.Append("</body></html>");
+
+            WebBrowser.NavigateToString(sb.ToString());
         }
     }
 }
