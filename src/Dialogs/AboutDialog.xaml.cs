ab277a475837bafeda9569aeaad6b80eea13ac12
Blake Pell <blakepell@hotmail.com>
2022-07-16 14:24:50 -0400

    Version info

--- src/Dialogs/AboutDialog.xaml.cs
+++ src/Dialogs/AboutDialog.xaml.cs
@@ -11,9 +11,24 @@ namespace LuaAutomation.Dialogs
 {
     public partial class AboutDialog
     {
+        public static readonly DependencyProperty VersionProperty = DependencyProperty.Register(
+            nameof(Version), typeof(string), typeof(AboutDialog), new PropertyMetadata(default(string)));
+
+        public string Version
+        {
+            get => (string)GetValue(VersionProperty);
+            set => SetValue(VersionProperty, value);
+        }
+
         public AboutDialog()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            string version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
+            string bit = Environment.Is64BitProcess ? "64-bit" : "32-bit";
+
+            this.Version = $"{version} {bit}";
         }
 
         private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
