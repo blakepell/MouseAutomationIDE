5f41aafdfd809ca1f5c15243fb94bab6ddd9e722
Blake Pell <blakepell@hotmail.com>
2022-07-17 16:34:30 -0400

    Start of examples.

---
+++ src/Pages/ExamplesPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Lua Automation IDE
+ *
+ * @project lead      : Blake Pell
+ * @website           : http://www.blakepell.com
+ * @copyright         : Copyright (c), 2022 All rights reserved.
+ * @license           : Closed Source
+ */
+
+namespace LuaAutomation.Pages
+{
+    public partial class ExamplesPage
+    {
+        public List<string> LuaExamples => new()
+        {
+            "while loop example", 
+            "for loop example"
+        };
+
+        public ExamplesPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ src/Resources/ExampleWhileLoop.lua
@@ -0,0 +1,13 @@
+﻿-- Count to 5 using a while loop and a control variable
+local count = 0
+
+while(count < 5)
+do
+    count = count + 1
+    ui.StatusText = "Loop " .. count
+    ui.ConsoleLog("Loop " .. count)    
+    ui.Sleep(1000)
+end
+
+ui.ConsoleLog("Complete!")
+ui.StatusText = "Complete!"
