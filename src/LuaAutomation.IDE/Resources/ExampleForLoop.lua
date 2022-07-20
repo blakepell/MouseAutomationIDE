442a6898d59b88d7e153bb7ace600fb33a5c04c6
Blake Pell <blakepell@hotmail.com>
2022-07-20 12:53:46 -0400

    For loop example update.

--- src/LuaAutomation.IDE/Resources/ExampleForLoop.lua
+++ src/LuaAutomation.IDE/Resources/ExampleForLoop.lua
@@ -14,4 +14,20 @@ for i = 10, 1, -1 do
     ui.Sleep(250)
 end
 
+-- Example of looping over an array with values
+local values = {
+    "Value 1",
+    "Value 2",
+    "Value 3",
+    "Value 4",
+    "Value 5"
+}
+
+-- i is the iteration of the loop you're on, if you don't need it
+-- you can replace it with a discard _
+for i, value in ipairs(values) do
+	ui.Log("Element " .. i .. " = " .. value)
+	ui.Sleep(250)
+end
+
 ui.Log("Complete!")
