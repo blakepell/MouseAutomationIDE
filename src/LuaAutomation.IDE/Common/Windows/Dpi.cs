664b51a496c941f97a777375a298b79835e52ae3
Blake Pell <blakepell@hotmail.com>
2022-07-20 13:53:24 -0400

    DPI stuff to see if it passes WACK

---
+++ src/LuaAutomation.IDE/Common/Windows/Dpi.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Lua Automation IDE
+ *
+ * @project lead      : Blake Pell
+ * @website           : http://www.blakepell.com
+ * @copyright         : Copyright (c), 2022 All rights reserved.
+ * @license           : Closed Source
+ */
+
+namespace LuaAutomation.Common.Windows
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static class Dpi
+    {
+        public static void SetProcessDpiAware()
+        {
+            _ = NativeMethods.SetProcessDPIAware();
+        }
+
+        public static void SetProcessDpiAware(ProcessDpiAwareness pda)
+        {
+            _ = NativeMethods.SetProcessDpiAwareness(pda);
+        }
+    }
+}
